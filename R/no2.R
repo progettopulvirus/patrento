0132043fd8b0fb0f0974d8eda977a3c5c3c4dced
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:12:59 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 5 stazioni della regione PATRENTO
+#'
+#' @format Un tibble con 8 colonne e 14000 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/pm10.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm10 per 5 stazioni della regione PATRENTO
+#'
+#' @format Un tibble con 8 colonne e 14000 osservazioni
+#'
+#' @usage
+#' pm10
+"pm10"
