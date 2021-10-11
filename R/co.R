afc17e4fa846a4416f35beefcc392ecdce14d2bc
guidofioravanti <guidofioravanti@yahoo.it>
2021-10-11 15:17:53 +0200

    Cambiati i criteri di selezione per le serie, adottati criteri utili al modello INLA sulle differenze

---
+++ R/co.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di co per 1 stazioni della regione PATRENTO
+#'
+#' @format Un tibble con 8 colonne e 731 osservazioni
+#'
+#' @usage
+#' co
+"co"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 3 stazioni della regione PATRENTO
+#'
+#' @format Un tibble con 8 colonne e 2193 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
